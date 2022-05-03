74e3e8daa35138dd4ef9d3013630d8417072e14e
Luigo3003 <joslutaes@gmail.com>
2022-05-03 01:49:04 -0500

    Se añadieron Respawn Points en el primer nivel y creación del cubo flotante

---
+++ Assets/Inside/Scripts/Mechanics/FrozenCube.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrozenCube : Interactable
+{
+    Rigidbody rb;
+
+    private bool isGrabed = false;
+    private void Start()
+    {
+        base.Start();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.E) && isGrabed)
+            Release();
+    }
+    public override void OnGrab(Transform parent)
+    {
+        rb.useGravity = false;
+        rb.isKinematic = true;
+        transform.SetParent(parent);
+        isGrabed = true;
+       
+    }
+
+    private void Release()
+    {
+
+        transform.SetParent(null);
+        isGrabed = false;
+    }
+
+    public bool IsGrabbed()
+    {
+        return isGrabed;
+    }
+}
